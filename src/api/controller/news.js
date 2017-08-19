487f4e2c0a68cd13979a73299f90aecd2ade1ffc
lizs <weshowapp@163.com>
2017-08-20 00:10:59 +0800

    Upd
    Upd

--- src/api/controller/news.js
+++ src/api/controller/news.js
@@ -15,6 +15,7 @@ export default class extends Base {
 	if (!think.isEmpty(news)) {
 	    let itemKey = 0;
 		for (let newsItem of news) {
+			//let videos1 = await this.model('video').join('user ON video.creator=user.id').where({news_id: newsItem.id}).order({'create_time': 'asc'}).select();
 			let videos = await this.model('video').where({news_id: newsItem.id}).order({'create_time': 'asc'}).select();
 			for (let i = 0; i < videos.length; i++) {
 				let user = await this.model('user').where({id: videos[i].creator}).select();
@@ -23,7 +24,15 @@ export default class extends Base {
 				    videos[i].creator_photo = user[0].photo_url;
 				}
 			}
+			if (!think.isEmpty(videos)) {
+				news[itemKey].updateDescrip = videos[0].creator_name + 'Update at' + videos[0].creator_time;
+			}
 			news[itemKey].video_list = videos;
+
+			let users = await this.model('video').where({news_id: newsItem.id}).getField('creator', 10);
+			let creatorList = await this.model('user').where({'id': {'in': users}}).select();
+			news[itemKey].creator_list = creatorList;
+			
 			news[itemKey].showVideo = false;
 			news[itemKey].curIndex = 0;
 			itemKey += 1;
