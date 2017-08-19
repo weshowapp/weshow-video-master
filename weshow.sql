a5b6aee5ec6417941d8ff8a833e9646a5d7fcdf9
lizs <weshowapp@163.com>
2017-08-19 09:42:02 +0800

    Update SQL
    Insert sql error.

--- weshow.sql
+++ weshow.sql
@@ -24,7 +24,7 @@ DROP TABLE IF EXISTS `weshow_user`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `weshow_user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `gender` smallint(1) unsigned NOT NULL DEFAULT '0',
+  `gender` smallint(2) unsigned NOT NULL DEFAULT '0',
   `account` varchar(32) NOT NULL DEFAULT '',
   `name` varchar(60) NOT NULL DEFAULT '',
   `photo_url` text NOT NULL,
@@ -42,12 +42,8 @@ CREATE TABLE `weshow_user` (
 
 LOCK TABLES `weshow_user` WRITE;
 /*!40000 ALTER TABLE `weshow_user` DISABLE KEYS */;
-INSERT INTO `weshow_user` 
-VALUES (1,'wx-1','Zhan',
-  'https://wx.qlogo.cn/mmopen/vi_32/bMqoLrFBhxP8ZsHJqOd2eIsEtpkCuSW0JtfOPZpsIaBKhqMaeONREia3R2TdtIf9oMAY2wq0pJPHUOCqzNXR7iaw/0',
-  0,1),
-  (1,'wx-2','Zhan1','https://wx.qlogo.cn/mmopen/vi_32/bMqoLrFBhxP8ZsHJqOd2eIsEtpkCuSW0JtfOPZpsIaBKhqMaeONREia3R2TdtIf9oMAY2wq0pJPHUOCqzNXR7iaw/0',
-  0,1),
+INSERT INTO `weshow_user` VALUES (1,'wx-1','Zhan','https://wx.qlogo.cn/mmopen/vi_32/bMqoLrFBhxP8ZsHJqOd2eIsEtpkCuSW0JtfOPZpsIaBKhqMaeONREia3R2TdtIf9oMAY2wq0pJPHUOCqzNXR7iaw/0',0,1),
+  (1,'wx-2','Zhan1','https://wx.qlogo.cn/mmopen/vi_32/bMqoLrFBhxP8ZsHJqOd2eIsEtpkCuSW0JtfOPZpsIaBKhqMaeONREia3R2TdtIf9oMAY2wq0pJPHUOCqzNXR7iaw/0',0,1),
   (1,'wx-3','Zhan2','http://yanxuan.nosdn.127.net/8e50c65fda145e6dd1bf4fb7ee0fcecc.jpg',0,1);
 /*!40000 ALTER TABLE `weshow_user` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -78,8 +74,7 @@ CREATE TABLE `weshow_video` (
 
 LOCK TABLES `weshow_video` WRITE;
 /*!40000 ALTER TABLE `weshow_video` DISABLE KEYS */;
-INSERT INTO `weshow_video` 
-VALUES ('Zhan','西二旗大街路面塌陷，轿车陷大坑四轮朝天',150,200,39.9181,116.3012,''),
+INSERT INTO `weshow_video` VALUES ('Zhan','西二旗大街路面塌陷，轿车陷大坑四轮朝天',150,200,39.9181,116.3012,''),
   ('Zhan1','八达岭长城关闭游览致高速拥堵数公里',150,200,39.9181,116.3012,'');
 /*!40000 ALTER TABLE `weshow_video` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -109,8 +104,7 @@ CREATE TABLE `weshow_news` (
 
 LOCK TABLES `weshow_news` WRITE;
 /*!40000 ALTER TABLE `weshow_news` DISABLE KEYS */;
-INSERT INTO `weshow_news` 
-VALUES ('西二旗大街路面塌陷，轿车陷大坑四轮朝天',1,100,300,39.9181,116.3012),
+INSERT INTO `weshow_news` VALUES ('西二旗大街路面塌陷，轿车陷大坑四轮朝天',1,100,300,39.9181,116.3012),
   ('八达岭长城关闭游览致高速拥堵数公里',1,100,300,39.9181,116.3012);
 /*!40000 ALTER TABLE `weshow_news` ENABLE KEYS */;
 UNLOCK TABLES;
