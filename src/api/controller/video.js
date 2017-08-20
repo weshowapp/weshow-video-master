50fc1a4ddbb2379de6769a5dc1032b34a61bfc5c
lizs <weshowapp@163.com>
2017-08-21 01:07:19 +0800

    Test
    Test

--- src/api/controller/video.js
+++ src/api/controller/video.js
@@ -22,7 +22,16 @@ export default class extends Base {
   async uploadAction(){
     //这里的 key 需要和 form 表单里的 name 值保持一致
     var file = think.extend({}, this.file('video'));
-
+	let title = this.post('title');
+    let create_time = this.post('create_time');
+    let longitude = this.post('longitude');
+    let latitude = this.post('latitude');
+    let address = this.post('address');
+    let creator_photo = this.post('creator_photo');
+    let creator_name = this.post('creator_name');
+    let creator_gender = this.post('creator_gender');
+	console.log(create_time);
+	
     var filepath = file.path;
 	console.log(file);
 	console.log(filepath);
@@ -42,6 +51,47 @@ export default class extends Base {
     }
 
     this.assign('fileInfo', file);
+	console.log(file.path);
+	
+	var fileUrl = "http://47.93.241.248/static/" + basename;
+	
+	let user = await this.model('user').where({photo_url: creator_photo, name: creator_name}).find();
+	if (think.isEmpty(user)) {
+	    let userResult = await this.model('user').add({
+            gender: creator_gender,
+            account: 'Wx'+creator_name,
+            name: creator_name,
+            photo_url: creator_photo,
+            reg_time: parseInt(new Date().getTime() / 1000)
+        });
+	    console.log(userResult);
+		user = await this.model('user').where({photo_url: creator_photo, name: creator_name}).find();
+	}
+	
+	let news = await this.model('news').where({latitude: latitude, longitude: longitude}).find();
+	if (think.isEmpty(news)) {
+	    let newsResult = await this.model('news').add({
+            title: title,
+            create_time: create_time,
+            longitude: longitude,
+            latitude: latitude,
+            location: address
+        });
+	    console.log(newsResult);
+		news = await this.model('news').where({latitude: latitude, longitude: longitude}).find();
+	}
+	
+	let videoResult = await this.model('user').add({
+        creator: user[0].id,
+		title: title,
+        news_id: news[0].id,
+        create_time: create_time,
+        longitude: longitude,
+        latitude: latitude,
+		poster_url: '',
+        video_url: fileUrl
+    });
+	console.log(videoResult);
 
     this.display();
   }
