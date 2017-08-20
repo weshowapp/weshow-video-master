66b3a69f32e9e789fcba62101d2725976db3263d
lizs <weshowapp@163.com>
2017-08-20 20:36:28 +0800

    Test
    Test

--- src/api/controller/video.js
+++ src/api/controller/video.js
@@ -16,4 +16,30 @@ export default class extends Base {
 
   }
 
+  async uploadAction(){
+    //这里的 key 需要和 form 表单里的 name 值保持一致
+    var file = think.extend({}, this.file('video'));
+
+    var filepath = file.path;
+	console.log(file);
+	console.log(filepath);
+
+    //文件上传后，需要将文件移动到项目其他地方，否则会在请求结束时删除掉该文件
+    var uploadPath = think.RESOURCE_PATH + '/upload';
+    think.mkdir(uploadPath);
+    var basename = path.basename(filepath);
+    fs.renameSync(filepath, uploadPath + '/' + basename);
+
+    file.path = uploadPath + '/' + basename;
+
+    if(think.isFile(file.path)){
+      console.log('is file')
+    }else{
+      console.log('not exist')
+    }
+
+    this.assign('fileInfo', file);
+
+    this.display();
+  }
 }
