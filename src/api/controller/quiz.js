61af0971a557d9ff44bffe6c6af00379e98917fb
weshowapp <31146098+weshowapp@users.noreply.github.com>
2018-01-28 06:53:56 +0800

    t

--- src/api/controller/quiz.js
+++ src/api/controller/quiz.js
@@ -46,7 +46,7 @@ export default class extends Base {
 			qidList.push(quizIdList[i].quizid);
 		}
         console.log(qidList);
-        list = await this.model('quiz').where({'quizid': ["IN", qidList]}).select();
+        list = await this.model('quiz').where({'id': ["IN", qidList]}).select();
 	    if (!think.isEmpty(list)) {
           console.log(list);
 		//for (var i = 0; i < list.length; i++) {
