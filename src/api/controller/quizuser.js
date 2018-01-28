83bc3261dd0a44ad5617608992135a4e52ebb05d
weshowapp <31146098+weshowapp@users.noreply.github.com>
2018-01-28 23:21:20 +0800

    t

--- src/api/controller/quizuser.js
+++ src/api/controller/quizuser.js
@@ -47,7 +47,7 @@ export default class extends Base {
   async getansweredAction() {
 	let quizid = this.get('quizid');
 	let question_id = this.get('question_id');
-    let info = await this.model('quizuser').where({quizid: quizid, question_id: question_id}).select();
+    let info = await this.model('quizuser').where({quizid: quizid, answer_status: question_id}).select();
 	if (!think.isEmpty(info)) {
 		for (var i = 0; i < info.length; i++) {
             let userInfo = await this.model('user').where({openid: info[i].openid}).find();
