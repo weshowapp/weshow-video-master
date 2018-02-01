a0db5971f506cc52237ff0633f3408be84e265bc
weshowapp <31146098+weshowapp@users.noreply.github.com>
2018-02-01 22:54:22 +0800

    time

--- src/api/controller/wxpay.js
+++ src/api/controller/wxpay.js
@@ -18,10 +18,23 @@ export default class extends Base {
   }
 
   async notifyAction() {
+    var http = require('http');
+	var server = http.createServer(function(req,res){
+		if(req.url!=="/favicon.ico"){
+			req.on('data',function(data){
+				console.log("RECEIVED:　"+decodeURIComponent(data));
+			});
+			req.on("end",function(){
+				console.log('客户端请求数据全部接收完毕');
+			});
+		}
+		res.end();
+	});
+	
     let return_code = this.post('return_code');
     let return_msg = this.post('return_msg');
 	var msg = 'msg-' + return_code + '-' + return_msg;
-	var tm = (new Date()).getTime();
+	var tm = ((new Date()).getTime()) / 1000;
 	
 		let addResult = await this.model('wxpay').add({
             notify: msg,
