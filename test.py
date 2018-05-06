701f8adf942e2db7514b3217414ce4a20ac826d5
weshowapp <31146098+weshowapp@users.noreply.github.com>
2018-05-06 14:41:40 +0800

    test

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 ﻿# -*- coding: UTF-8 -*-  
 import urllib  
 import urllib2
+import re
 from bs4 import BeautifulSoup  
+import datetime
+import time
   
 #下载数据  
 url = "http://www.weather.com.cn/weather/101260101.shtml"  
@@ -17,4 +20,21 @@ for u in name:
     content = "天气:" + wea + " 温度:" + tem  
     content = content.replace("\n","")  
     print(content)
+
+    if 1:
+        #nowTime=datetime.datetime.now().microsecond
+        nowTime=time.time()
+        strTime='3天前'
+        tm = re.match(r'(.*)(分钟|小时|天)前', strTime, re.M|re.I)
+        print 'nowTime'
+        print tm
+        print nowTime
+        if tm:
+            if (tm.group(1).indexOf('分钟') != -1):
+                nowTime = nowTime - int(tm[1]) * 60
+            elif (tm.group(1).indexOf('小时') != -1):
+                nowTime = nowTime - int(tm[1]) * 60 * 60
+            elif (tm.group(1).indexOf('天') != -1):
+                nowTime = nowTime - int(tm[1]) * 60 * 60 * 24
+        print nowTime
 
