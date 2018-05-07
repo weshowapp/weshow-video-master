81052c370cacad47067d1ed901e770cd5da86e71
weshowapp <31146098+weshowapp@users.noreply.github.com>
2018-05-07 22:36:29 +0800

    time

--- src/api/controller/article.js
+++ src/api/controller/article.js
@@ -25,6 +25,7 @@ export default class extends Base {
     if (!think.isEmpty(data)) {
         await this.model('article').setMagazine(data);
         await this.model('article').setLikeList(data, openid);
+        data.pub_time_str = this.model('article').getCurTimeStamp(data.pub_time);
         var nextdata = await this.model('article').where({id: nextid}).find();
         if (!think.isEmpty(nextdata)) {
           data.next_title = nextdata.title;
@@ -70,6 +71,7 @@ export default class extends Base {
       for (var i = 0; i < list.length; i++) {
         await this.model('article').setMagazine(list[i]);
         await this.model('article').setLikeList(list[i], openid);
+        list[i].pub_time_str = this.model('article').getCurTimeStamp(list[i].pub_time);
       }
     }
     return this.success({
@@ -79,7 +81,7 @@ export default class extends Base {
 
   async getuserlikesAction() {
     let openid = this.get('openid');
-    var likeList = await this.model('comment').field('artid').where({openid: openid, up: 1}).limit(30).select();
+    var likeList = await this.model('comment').field('artid').where({openid: openid, up: 1}).limit(15).select();
     var aidList = [];
     if (!think.isEmpty(likeList)) {
         for (var i = 0; i < likeList.length; i++) {
@@ -92,6 +94,7 @@ export default class extends Base {
       for (var i = 0; i < list.length; i++) {
         await this.model('article').setMagazine(list[i]);
         await this.model('article').setLikeList(list[i], openid);
+        list[i].pub_time_str = this.model('article').getCurTimeStamp(list[i].pub_time);
       }
     }
     return this.success({
@@ -102,7 +105,7 @@ export default class extends Base {
   async getbysourceAction() {
     let openid = this.get('openid');
     let srcname = this.get('sourcename');
-    let list = await this.model('article').where({'source_name': srcname}).limit(30).select();
+    let list = await this.model('article').where({'source_name': srcname}).limit(15).select();
     if (!think.isEmpty(list)) {
       //console.log(list);
       for (var i = 0; i < list.length; i++) {
